762c29c1b3b04970cefb7551306d727e1255fa48
skdbsqls <skdbsqls98@naver.com>
2023-04-10 18:00:11 +0900

    반복문 연습

--- Baekjoon/반복문/15552.js
+++ Baekjoon/반복문/15552.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-const input = fs.readFileSync('Baekjoon/example.txt').toString().trim().split("\n")
+const input = fs.readFileSync(0).toString().trim().split("\n")
 
 const T = Number(input[0])
 
 for (let i=1; i<=T; i++) {
     const [ A, B ] = input[i].toString().trim().split(" ").map((x)=>Number(x))
+    console.log( A+B )
 }
-console.log( A+B )
