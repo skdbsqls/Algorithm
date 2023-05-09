aa3eff087a9dd33067723ae4f18bc33174e686a4
skdbsqls <skdbsqls98@naver.com>
2023-05-09 20:33:19 +0900

    자바스크립트 속성 강의

--- js속성가이드/01variable.js
+++ js속성가이드/01variable.js
@@ -1,13 +1,10 @@
-// 변수와 상수
 
-// 변수 let
-let name;
-name =  'kindtiger'
+let name
+let age = 25
 
-// 상수 const
-const age;
-age = 25
+name = 'kindtiger'
 
+console.log(name)
 console.log(age)
-// 변수 let은 비워두고 나중에 할당하는 것이 가능하지만, 상수 const는 불가능하다.
+
 
